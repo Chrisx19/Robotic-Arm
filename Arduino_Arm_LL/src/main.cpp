99c7ff6e934b198509dbf06d9f2e38d28505276d
Chrisx19 <Christopherlazo1619@gmail.com>
2023-05-01 22:28:13 -0700

    Final for Join_msg

--- Arduino_Arm_LL/src/main.cpp
+++ Arduino_Arm_LL/src/main.cpp
@@ -4,7 +4,7 @@
 #include <std_msgs/Bool.h>
 #include <SoftwareSerial.h>
 #include <math.h>
-
+#include <arm_package/Floats_array.h>
 #include <VarSpeedServo.h>
 #include <AccelStepper.h>
 
@@ -12,19 +12,19 @@ AccelStepper stepper1(1, 2, 3); //base
 AccelStepper stepper2(1, 4, 5); //shoulder
 VarSpeedServo elbow_servo, wrist_servo; 
 
-const int analogInPin = A0;
+const int elbow_analogPin = A0;
+const int wrist_analogPin = A1;
 int sensorValue = 0; 
-
-
-// const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 
 bool  g_Joints_EN_Joy = false;  //global default for joint enable
 const int Joints_EN_Pin = 13;   //out pin @ arduino mega
 
 ros::NodeHandle  nh;                                          //object 
 void arm_joint_cb(const arm_package::Joints& Joints_data);   //init function
+void callback(const arm_package::Floats_array::Request & req, arm_package::Floats_array::Response & res);
 
 ros::Subscriber<arm_package::Joints> Arm_joint_sub("joints", &arm_joint_cb);       //ros sub
+ros::ServiceServer<arm_package::Floats_array::Request, arm_package::Floats_array::Response> server("/read_joint_state",&callback);
 
 std_msgs::Float64 Test;
 ros::Publisher test_pub("test", &Test);
@@ -40,9 +40,8 @@ void setup()
   stepper1.setMaxSpeed(1000000);
   stepper2.setMaxSpeed(1000000);
   elbow_servo.attach(9);
+  wrist_servo.attach(10);
   // elbow_servo.write(34);  //90~ 0degree is at home, 149~90 degree  34~-90 degrees
-
-  
 }
 
 void loop()
@@ -81,6 +80,12 @@ That means we can manually move the motor with our hands and read the encoder va
   //  delay(1);
 }
 
+// ros::ServiceServer<arm_package::Floats_array::Request, arm_package::Floats_array::Response> server("/read_joint_state",&callback)
+// {
+
+// }
+
+
 void arm_joint_cb(const arm_package::Joints& Joints_data)  //callback function from subscribe on driving each joins
 {
   float base_joint = Joints_data.Joint_1;       //-1.57 <-> 1.57
@@ -95,23 +100,18 @@ void arm_joint_cb(const arm_package::Joints& Joints_data)  //callback function f
 
   float base = map(base_degree, -90, 90, -1000, 1000);
   float shoulder = map(shoulder_degree, -90, 90, -1000, 1000);
-  float elbow = map(elbow_degree, -90, 90, 37, 142);
+  float elbow = map(elbow_degree, -90, 90, 34, 145);
   // float wrist = map(wrist_degree, -90, 90, 34, 149);
 
   Test.data = base;
   test_pub.publish(&Test);
 
-  if (Joints_data.EN)
-  {
-    g_Joints_EN_Joy = true;
-  }
-  else
-  {
-    g_Joints_EN_Joy = false;
-  }
+  g_Joints_EN_Joy = Joints_data.EN;
+
+  elbow = 0; 
 
   stepper1.setSpeed(base);
   stepper2.setSpeed(shoulder);
-  elbow_servo.write(elbow, 20);
-  // wrist_servo.write(wrist);
+  elbow_servo.slowmove(elbow, 15);
+  wrist_servo.slowmove(0, 15);
 }
