5ae352e20e9514d40a0fcf16fc58ef0e847de44c
Chrisx19 <Christopherlazo1619@gmail.com>
2023-05-11 23:44:08 -0700

    Servo control

--- Arduino_Arm_LL/src/main.cpp
+++ Arduino_Arm_LL/src/main.cpp
@@ -16,6 +16,10 @@ int16_t elbow_val_raw = 0,
         wrist_val_raw = 0,
         wrist_roll_val_raw = 0; 
 
+float elbow = 0,
+      wrist = 0,
+      wrist_roll = 0;
+
 AccelStepper    waist_joint1(1, 2, 3), //base
              shoulder_joint2(1, 4, 5); //shoulder
 
@@ -29,17 +33,16 @@ const int Joints_EN_Pin = 13;   //out pin @ arduino mega
 ros::NodeHandle  nh;                                          //object 
 void arm_joint_cb(const arm_package::Joints& Joints_data);   //init function
 void read_analog(void);
-void callback(const arm_package::Floats_array::Request & req, arm_package::Floats_array::Response & res);
 
 ros::Subscriber<arm_package::Joints> Arm_joint_sub("joints", &arm_joint_cb);       //ros sub
 
-// std_msgs::Float64 Test;
-// ros::Publisher test_pub("test", &Test);
+std_msgs::Float64 Test;
+ros::Publisher test_pub("test", &Test);
 
 void setup()
 {
   nh.initNode();
-  // nh.advertise(test_pub);
+  nh.advertise(test_pub);
   nh.subscribe(Arm_joint_sub);
 
   // Serial.begin(9600);
@@ -50,6 +53,7 @@ void setup()
   wrist_joint4.attach(7);
   wrist_roll_joint5.attach(8);
   // elbow_servo.write(34);  //90~ 0degree is at home, 149~90 degree  34~-90 degrees
+
 }
 
 void loop()
@@ -74,9 +78,11 @@ That means we can manually move the motor with our hands and read the encoder va
   waist_joint1.runSpeed();
   shoulder_joint2.runSpeed();
 
-  elbow_joint3.slowmove(90, 15);
-  wrist_joint4.slowmove(90, 15);
-  wrist_roll_joint5.slowmove(90, 15);
+  // elbow_joint3.slowmove(90, 15);
+  // wrist_joint4.slowmove(90, 15);
+  // wrist_roll_joint5.slowmove(90, 15);
+
+  test_pub.publish(&Test);
 
   nh.spinOnce();
   //  delay(1);
@@ -84,13 +90,13 @@ That means we can manually move the motor with our hands and read the encoder va
 
 void arm_joint_cb(const arm_package::Joints& Joints_data)  //callback function from subscribe on driving each joins
 {
-
   g_Joints_EN_Joy = Joints_data.EN;
 
   float base_joint = Joints_data.Joint_1;       //-1.57 <-> 1.57
   float shoulder_joint = Joints_data.Joint_2;  //-1.57 <-> 1.57
-  // float elbow_joint = Joints_data.Joint_3;     //-1.57 <-> 1.57
-  // float wrist_joint = Joints_data.Joint_4;     //-1.57 <-> 1.57
+  float elbow_joint = Joints_data.Joint_3;     //-1.57 <-> 1.57
+  float wrist_joint = Joints_data.Joint_4;     //-1.57 <-> 1.57
+  float wrist_roll_joint = Joints_data.Joint_5;     //-1.57 <-> 1.57
 
   float base_degree = (base_joint * 180)/ PI;         // -90 <-> 90
   float shoulder_degree = (shoulder_joint * 180)/ PI;
@@ -99,15 +105,50 @@ void arm_joint_cb(const arm_package::Joints& Joints_data)  //callback function f
 
   float base = map(base_degree, -90, 90, -1000, 1000);
   float shoulder = map(shoulder_degree, -90, 90, -1000, 1000);
-  // float elbow = map(elbow_degree, -90, 90, 34, 145);
+  // float elbow = map(elbow_degree, -90, 90, 36, 140);  //position
   // float wrist = map(wrist_degree, -90, 90, 34, 149);
 
-  // Test.data = base;
-  // test_pub.publish(&Test);
+  //=====================================================
+  elbow += elbow_joint; //we have to send min 36 and max is 140
+  wrist += wrist_joint; //we have to send min 36 and max is 140
+  wrist_roll += wrist_roll_joint; //we have to send min 36 and max is 140
+
+  float elbow_final = elbow + 88;
+  float wrist_final = wrist + 88.5;
+  float wrist_roll_final = wrist_roll + 90;
+
+  if ( elbow_final <= 36) {      //elbow joint3
+    elbow_final = 36;
+  }
+  else if (elbow_final >= 140){
+    elbow_final = 140;
+  }
+
+  if ( wrist_final <= 34) {      //wrist joint4
+    wrist_final = 34;
+  }
+  else if (wrist_final >= 143){
+    wrist_final = 143;
+  }
+
+  if ( wrist_roll_final <= 35) {      //wrist_roll joint5
+    wrist_roll_final = 35;
+  }
+  else if (wrist_roll_final >= 145){
+    wrist_roll_final = 145;
+  }
+  
+  //===========================================================
 
   waist_joint1.setSpeed(base);
   shoulder_joint2.setSpeed(shoulder);
 
+  elbow_joint3.slowmove(elbow_final, 15); //88 mid
+  wrist_joint4.slowmove(wrist_final, 15);
+  wrist_roll_joint5.slowmove(wrist_roll_final, 15);
+
+  Test.data = elbow_final;
+  
 }
 
 void read_analog(void)
