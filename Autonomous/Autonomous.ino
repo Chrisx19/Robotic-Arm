b823023fd1b1bfde8e2dfb9c0fb9d0227feb9fcf
Chris Lazo <107272321+Chrisx19@users.noreply.github.com>
2023-05-17 03:53:17 -0700

    Update Autonomous.ino

--- Autonomous/Autonomous.ino
+++ Autonomous/Autonomous.ino
@@ -18,7 +18,7 @@ int16_t waist_val_raw      = 0,
         wrist_roll_val_raw = 0;        // value read from the pot
         
 
-int8_t  waist_cmd      = 0,
+int16_t waist_cmd      = 0,
         shoulder_cmd   = 0,
         elbow_cmd      = 0,
         wrist_cmd      = 0,        // value output to the PWM (analog out)
@@ -53,10 +53,11 @@ void setup() {
 //  joint[wrist]      = 88.5;
 //  joint[wrist_roll] = 90;
 
-  waist_cmd      = 50;
-  shoulder_cmd   = -30;
+  waist_cmd      = 159;
+  shoulder_cmd   = 287;
+  
   elbow_cmd      = 0;
-  wrist_cmd      = 0;
+  wrist_cmd      = -90;
   wrist_roll_cmd = 0;
   
 }
@@ -67,20 +68,19 @@ void loop() {
   // read the value from the sensor:
   read_analog();
 
-  g_Joints_disable_Joy = true;
+  g_Joints_disable_Joy = false;
+//  g_Joints_disable_Joy = true;
   disable_motors(g_Joints_disable_Joy);
   
   waist_joint1.runSpeed();
   shoulder_joint2.runSpeed();
 
-  uint16_t joint_adc_min[5] = {160, 268, 36, 32, 34};
-  uint16_t joint_adc_max[5] = {221, 302, 129, 127, 133};
+  uint16_t joint_adc_min[5] = {107, 268, 36, 32, 34};
+  uint16_t joint_adc_max[5] = {177, 302, 129, 127, 133};
 
-//  int16_t waist =      map(waist_cmd,      -30, 90, 302, 36);
-  int16_t shoulder =   map(shoulder_cmd,   -30, 90, 302, 268);
-  int16_t elbow =      map(elbow_cmd,       90, -90, 36, 140);          //min 35(servo write cmd)~ -90 degree ; max 140~ 90 degree/////////////MID ~ 88
-  int16_t wrist =      map(wrist_cmd,      -90, 90, 34, 143);          //min 34~ -90 degree ; max 143~ 90 degree
-  int16_t wrist_roll = map(wrist_roll_cmd, -90, 90, 35, 145);          //min 35~ -90 degree ; max 135~ 90 degree
+  uint16_t elbow =      map(elbow_cmd,      -90, 90, 36, 140);          //min 35(servo write cmd)~ -90 degree ; max 140~ 90 degree/////////////MID ~ 88
+  uint16_t wrist =      map(wrist_cmd,       90, -90, 34, 143);          //min 34~ -90 degree ; max 143~ 90 degree
+  uint16_t wrist_roll = map(wrist_roll_cmd, -90, 90, 35, 145);          //min 35~ -90 degree ; max 135~ 90 degree
 
 // There was a bug here before when it was using void but not its good with return func
   waist_val_raw      = max_min(waist_val_raw,      joint_adc_min[0], joint_adc_max[0]);          //raw_val from ADC, minimum val_adc, maximum val_adc
@@ -89,42 +89,49 @@ void loop() {
   wrist_val_raw      = max_min(wrist_val_raw,      joint_adc_min[3], joint_adc_max[3]);          
   wrist_roll_val_raw = max_min(wrist_roll_val_raw, joint_adc_min[4], joint_adc_max[4]);
 
-  int16_t  shoulder_degree =   map(shoulder_val_raw,   joint_adc_min[1], joint_adc_max[1], -30, 90); 
   int16_t  elbow_degree =      map(elbow_val_raw,      joint_adc_min[2], joint_adc_max[2],  90, -90); 
   int16_t  wrist_degree =      map(wrist_val_raw,      joint_adc_min[3], joint_adc_max[3], -90, 90);
   int16_t  wrist_roll_degree = map(wrist_roll_val_raw, joint_adc_min[4], joint_adc_max[4], -90, 90);
 
 //*********************************Motor Commands***********************************
-  stepper_position_1(waist_val_raw, waist_cmd, 500 ,10);
-  stepper_position_2(shoulder_val_raw, shoulder_cmd, 500 ,10);
-  elbow_joint3.slowmove(elbow, 15);
-  wrist_joint4.slowmove(wrist, 15);
-  wrist_roll_joint5.slowmove(wrist_roll, 15);
+//  stepper_position_1(waist_val_raw, waist_cmd, 1000 ,3);
+//  stepper_position_2(shoulder_val_raw, shoulder_cmd, 500 ,25);
+  elbow_joint3.slowmove(elbow, 10);
+  wrist_joint4.slowmove(wrist, 10);
+  wrist_roll_joint5.slowmove(wrist_roll, 10);
 //*********************************Motor Commands***********************************
 
 
-//    Serial.print("       Raw_val = ");       //debug for Testing 
-//    Serial.print(shoulder_val_raw);
-//    Serial.print("\t");
-//    Serial.print("       Degree = ");
-//    Serial.println(shoulder_degree);
+    Serial.print("Raw_val = ");       //debug for Testing 
+    Serial.print(waist_val_raw);
+    Serial.print("\t");
+    Serial.print("Target = ");
+    Serial.print(waist_cmd);
+    Serial.print("\t");
+    
 
-//  Serial.print("       Test = ");
-//  Serial.println(shoulder);
-//
-//  Serial.print("       wrist  = ");
-//  Serial.print(wrist_degree);
-//  
-//  Serial.print("       Degree = ");
-//  Serial.println(waist);
-//  delay(100);
+    Serial.print("Raw_val = ");       //debug for Testing 
+    Serial.print(shoulder_val_raw);
+    Serial.print("\t");
+    Serial.print("Target = ");
+    Serial.print(shoulder_cmd);
+    Serial.print("\t");
+
+
+    Serial.print("Raw_val = ");       //debug for Testing 
+    Serial.print(elbow_degree);
+    Serial.print("\t");
+    Serial.print("Target = ");
+    Serial.print(elbow_cmd);
+    Serial.println("\t");
+
 
 }
 //#################################END LOOOP  FUNCTION################################
 
 
 //#######################START OF FUNCTION IMPLEMENT##################################
-double g_decel = 500;
+double g_decel = 1000;
 void stepper_position_1(int sensor, int target, int velocity,int threshold)
 {
   
@@ -140,8 +147,16 @@ void stepper_position_1(int sensor, int target, int velocity,int threshold)
   }
   else
   {
-    g_decel = velocity;
-    waist_joint1.setSpeed(velocity);
+    
+     if( target > sensor)
+     {
+        waist_joint1.setSpeed(-1*velocity);
+     }
+     else
+     {
+        waist_joint1.setSpeed(velocity);
+     }
+      
   }
 //    Serial.print(g_decel);      //Debug for value decel
 //    Serial.print("    ");
@@ -158,13 +173,13 @@ void stepper_position_2(int sensor, int target, int velocity,int threshold)
     {
       g_decel = 0;
     }
-    waist_joint1.setSpeed(g_decel);
+    shoulder_joint2.setSpeed(g_decel);
     delay(30);
   }
   else
   {
     g_decel = velocity;
-    waist_joint1.setSpeed(velocity);
+    shoulder_joint2.setSpeed(velocity);
   }
 //    Serial.print(g_decel);      //Debug for value decel
 //    Serial.print("    ");
@@ -206,25 +221,25 @@ uint16_t g_target_adc_raw[5] = {500, 500, 155, 155, 155};
 void read_analog(void)
 { 
   //*********************************Stepper Analog***********************************
-  waist_val_raw      = analogRead(waist_pin) / (1023 / g_target_adc_raw[0]);
+  waist_val_raw      = analogRead(waist_pin)    / (1023 / g_target_adc_raw[0]);
   shoulder_val_raw   = analogRead(shoulder_pin) / (1023 / g_target_adc_raw[1]);
   //*********************************Stepper Analog***********************************
   
   //*********************************Servo Analog***********************************
-  elbow_val_raw      = analogRead(elbow_pin) / (685 / g_target_adc_raw[2]);
-  wrist_val_raw      = analogRead(wrist_pin) / (685 / g_target_adc_raw[3]);  //685 max
+  elbow_val_raw      = analogRead(elbow_pin)      / (685 / g_target_adc_raw[2]);
+  wrist_val_raw      = analogRead(wrist_pin)      / (685 / g_target_adc_raw[3]);  //685 max
   wrist_roll_val_raw = analogRead(wrist_roll_pin) / (685 / g_target_adc_raw[4]);
   //*********************************Servo Analog***********************************
 
 //  *********************************Debug for Analog***********************************
-  Serial.print(waist_val_raw);      
-  Serial.print("\t");
-  Serial.print(shoulder_val_raw);      
-  Serial.print("\t");
-  Serial.print(elbow_val_raw);      
-  Serial.print("\t");
-  Serial.print(wrist_val_raw); 
-  Serial.print("\t");
-  Serial.println(wrist_roll_val_raw);  
+//  Serial.print(waist_val_raw);      
+//  Serial.print("\t");
+//  Serial.print(shoulder_val_raw);      
+//  Serial.print("\t");
+//  Serial.print(elbow_val_raw);      
+//  Serial.print("\t");
+//  Serial.print(wrist_val_raw); 
+//  Serial.print("\t");
+//  Serial.println(wrist_roll_val_raw);  
 
 }
